566484daecef21cd883958a37c68bc38912dbd1f
thaku <thakuraditya696@gmail.com>
2021-04-08 22:57:36 +0530

    Second Commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuidingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuidingblocksApplication.java
@@ -2,12 +2,12 @@ package com.stacksimplify.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class SpringbootBuidingblocksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuidingblocksApplication.class, args);
 	}
-
+//
 }
